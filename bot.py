c282e753b9ceecb55aa492ea44ddd8210d48d77c
Piorum <90207649+Piorum@users.noreply.github.com>
2021-11-18 01:00:49 -0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import time
 import math
+import random
 import keyboard
 import pyautogui
 import cv2 as cv
@@ -11,31 +12,44 @@ pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 output = '1'
 outputL = '1'
+def checknumber():
+    global output
+    img = pyautogui.screenshot(region=(380, 927, 60, 20))
+    new_size=tuple(4*x for x in img.size)
+    img = img.resize(new_size)
+    img.save('ScreenshotRaw.png')
+    img = cv.imread('ScreenshotRaw.png',0)
+    ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
+    blur = cv.GaussianBlur(thresh2,(5,5),0)
+    ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    cv.imwrite('ScreenshotProccesed.png', th3)
+    output = pytesseract.image_to_string(th3)
+    symbols = ['i', 'l', '|', '!', 'I', ')', '(', ':', ';', ']', '[', '{', '}']
+    for x in symbols:
+        output = output.replace(x,'1')
+    numeric_filter = filter(str.isdigit, output)
+    output = "".join(numeric_filter)
 
 while True:
-    time.sleep(2)
-    try:
-        img = pyautogui.screenshot(region=(380, 936, 60, 20))
-        new_size=tuple(4*x for x in img.size)
-        img = img.resize(new_size)
-        img.save('ScreenshotRaw.png')
-        img = cv.imread('ScreenshotRaw.png',0)
-        ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
-        blur = cv.GaussianBlur(thresh2,(5,5),0)
-        ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-        cv.imwrite('ScreenshotProccesed.png', th3)
-        output = pytesseract.image_to_string(th3)
-        symbols = ['i', 'l', '|', '!', 'I']
-        for x in symbols:
-            output = output.replace(x,'1')
-        numeric_filter = filter(str.isdigit, output)
-        output = "".join(numeric_filter)
-        if len(output) == 6:
-            if int(outputL) != int(output):
-                output = int(output) + 1
-                keyboard.write(str(output))
+    firstsleep = random.randint(2, 4)
+    time.sleep(firstsleep)
+    checknumber()
+    if len(output) == 6:
+        if int(outputL) != int(output):
+            output = int(output) + 1
+            outputL = output
+            keyboard.write(str(output))
+            time.sleep(1.2)
+            checknumber()
+            output = int(output) + 1
+            if int(output) == int(outputL):
                 keyboard.press_and_release('enter')
-                outputL = output
                 time.sleep(11)
-    except:
-        print("ERROR: '" + str(output) + "' '" + str(outputL) + "'")
+            else:
+                if len(str(output)) == 6:
+                    keyboard.press_and_release('ctrl+a')
+                    keyboard.write(str(output))
+                    keyboard.press_and_release('enter')
+                    outputL = output
+                    time.sleep(11)
+                
