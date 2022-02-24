08c29913f8568b030c9a346f750b1443e90ee986
Piorum <90207649+Piorum@users.noreply.github.com>
2022-02-24 04:05:10 -0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -12,6 +12,10 @@ pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 output = '1'
 outputL = '1'
+firstsleep = '1'
+breaktrigger = random.randint(10,20)
+breakcount = 0
+delaycheck = 0
 def checknumber():
     global output
     img = pyautogui.screenshot(region=(380, 927, 60, 20))
@@ -30,26 +34,71 @@ def checknumber():
     numeric_filter = filter(str.isdigit, output)
     output = "".join(numeric_filter)
 
+def breaktime():
+    global breaktrigger
+    global breakcount
+    breakcount += 1
+    if breakcount >= breaktrigger:
+        sleeptime = random.randint(12600,24200)
+        print("Sleeping for " + str(sleeptime) + " seconds.")
+        time.sleep(sleeptime)
+        breaktrigger = random.randint(250,300)
+        breakcount = 0
+        print("Sleep is over.")
 while True:
-    firstsleep = random.randint(2, 4)
-    time.sleep(firstsleep)
-    checknumber()
-    if len(output) == 6:
-        if int(outputL) != int(output):
-            output = int(output) + 1
-            outputL = output
-            keyboard.write(str(output))
-            time.sleep(1.2)
-            checknumber()
-            output = int(output) + 1
-            if int(output) == int(outputL):
+    try:
+        firstsleep = random.randint(2, 3)
+        time.sleep(firstsleep)
+        checknumber()
+        if len(output) == 6:
+            if delaycheck >= 24:
+                time.sleep(delaycheck)
+                keyboard.press_and_release('ctrl+a')
+                keyboard.write(str(output))
                 keyboard.press_and_release('enter')
-                time.sleep(11)
-            else:
-                if len(str(output)) == 6:
-                    keyboard.press_and_release('ctrl+a')
-                    keyboard.write(str(output))
+                time.sleep(9)
+                breaktime()
+                delaycheck = 0
+            if int(outputL) != int(output):
+                time.sleep(delaycheck)
+                output = int(output) + 1
+                outputL = output
+                keyboard.press_and_release('ctrl+a')
+                keyboard.write(str(output))
+                time.sleep(2)
+                checknumber()
+                output = int(output) + 1
+                if int(output) == int(outputL):
                     keyboard.press_and_release('enter')
-                    outputL = output
-                    time.sleep(11)
+                    time.sleep(9)
+                    breaktime()
+                    delaycheck = 0
+                else:
+                    if len(str(output)) == 6:
+                        keyboard.press_and_release('ctrl+a')
+                        keyboard.write(str(output))
+                        keyboard.press_and_release('enter')
+                        outputL = output
+                        time.sleep(9)
+                        breaktime()
+                        delaycheck = 0
+                    else:
+                        delaycheck += random.randint(0,3)
+            else:
+                delaycheck += random.randint(0,3)
+    except:
+        print('ERROR')
+        print('output: ' + str(output))
+        print('outputL: ' + str(outputL))
+        print('firstsleep: ' + str(firstsleep))
+        print('breaktrigger: ' + str(breaktrigger))
+        print('breakcounter: ' + str(breakcount))
+        print('delaycheck: ' + str(delaycheck))
+        try:
+            keyboard.press_and_release('ctrl+a')
+            time.sleep(15)
+            breaktime()
+        except:
+            print('***CRITICAL ERROR ABORT ABORT ABORT***')
+        
                 
